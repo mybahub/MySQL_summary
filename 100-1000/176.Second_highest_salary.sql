145f2afce67fe9cd15c4337dfcc1f9da19625d99
mybahub <enochyn@outlook.com>
2021-08-31 15:36:12 -0700

    update 176

--- 100-1000/176.Second_highest_salary.sql
+++ 100-1000/176.Second_highest_salary.sql
@@ -2,6 +2,17 @@
 https://leetcode.com/problems/second-highest-salary/
 */
 
+/* Solution 1 */
+
 SELECT max(salary) AS SecondHighestSalary
 FROM Employee
 WHERE salary<(SELECT max(salary) FROM Employee);
+
+
+/* Solution 2 */
+
+SELECT
+(SELECT distinct salary
+FROM Employee
+ORDER BY salary DESC
+LIMIT 1,1) AS SecondHighestSalary
