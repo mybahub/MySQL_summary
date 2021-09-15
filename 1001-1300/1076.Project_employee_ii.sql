881166208bc738e173a636bdb7c9770f50d3f9bc
mybahub <enochyn@outlook.com>
2021-09-14 20:32:09 -0700

    update 1076

--- 1001-1300/1076.Project_employee_ii.sql
+++ 1001-1300/1076.Project_employee_ii.sql
@@ -1,9 +1,43 @@
-/* Easy */
+/* Easy
+https://leetcode.com/problems/project-employees-ii/submissions/
+*/
 
-/* Solution
-where count >= all() to get the max
+/* Solution 1
+*/
 
-from KumaG
+WITH mnum AS(
+    SELECT count(1)
+    FROM Project
+    GROUP BY project_id
+    ORDER BY count(1) DESC
+    LIMIT 1
+)
+
+
+SELECT project_id
+FROM Project
+GROUP BY project_id
+HAVING count(1)=(SELECT * FROM mnum);
+
+/* Solution 2
+*/
+WITH summary AS (
+    SELECT
+        project_id,
+        rank() OVER(ORDER BY count(employee_id) DESC) AS rn
+    FROM Project
+    GROUP BY project_id
+)
+
+SELECT project_id
+FROM summary
+WHERE rn=1
+
+
+
+/* Solution 3
+where count >= all() to get the max from KumaG
+slow because group by in the where condition run multiple times
 */
 
 select project_id
