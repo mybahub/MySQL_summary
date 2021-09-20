c7d2bc0d58fdcf227d34ce2710f93023be6af0a2
mybahub <enochyn@outlook.com>
2021-09-19 18:45:31 -0700

    update 1098

--- 1001-1300/1098.Unpopular_books.sql
+++ 1001-1300/1098.Unpopular_books.sql
@@ -18,25 +18,28 @@ WHERE available_from <=DATE_SUB("2019-06-23", INTERVAL 1 MONTH) AND book_id NOT 
 
 
 /* Solution 2
-from PeterHontaru
+
 left join+ filter in ON
 */
 
-SELECT b.book_id, b.name
-FROM Books as b
-LEFT JOIN Orders as o
-    ON b.book_id = o.book_id AND o.dispatch_date >= DATEADD(year, -1, '2019-06-23')
-WHERE b.available_from <= DATEADD(month, -1, '2019-06-23')
-GROUP BY b.name, b.book_id
-HAVING COALESCE(SUM(o.quantity), 0) < 10
+SELECT
+    b.book_id,
+    b.name
+FROM Books b
+LEFT JOIN Orders o
+USING(book_id)
+WHERE b.available_from<=DATE_SUB('2019-06-23',INTERVAL 1 MONTH )
+GROUP BY b.book_id
+HAVING sum(if(o.dispatch_date>DATE_SUB('2019-06-23', INTERVAL 1 YEAR),o.quantity,0))<10;
 
 /* Solution 3
 left join+ if/case when
 */
 SELECT B.book_id, B.name
 
-FROM Books B LEFT JOIN Orders O USING(book_id)
+FROM Books B
+LEFT JOIN Orders O
+USING(book_id)
 WHERE available_from <= DATE_SUB('2019-06-23', INTERVAL 1 MONTH)
-
 GROUP BY B.book_id
-HAVING SUM(CASE WHEN dispatch_date >= DATE_SUB('2019-06-23', INTERVAL 1 YEAR) THEN quantity ELSE 0 END) < 10
+HAVING SUM(CASE WHEN O.dispatch_date >= DATE_SUB('2019-06-23', INTERVAL 1 YEAR) THEN O.quantity ELSE 0 END) < 10
