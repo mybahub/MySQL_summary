19116ecba196505fc5d6fc3c8e6c28af5746e7e0
mybahub <enochyn@outlook.com>
2021-10-05 12:47:10 -0700

    update 1205

--- 1001-1300/1205.Monthly_transction_ii.sql
+++ 1001-1300/1205.Monthly_transction_ii.sql
@@ -4,44 +4,48 @@
 FUll join= left join +right join+union
 */
 
-WITH trans_by_month AS (
-    SELECT DATE_FORMAT(trans_date,"%Y-%m") AS month,
-    country,
-    count(country) AS approved_count ,
-    sum(amount) AS approved_amount
+WITH trans_summary AS (
+    SELECT
+        DATE_FORMAT(trans_date,'%Y-%m') AS month,
+        country,
+        count(id) AS approved_count,
+        sum(amount) AS approved_amount
     FROM Transactions
-    WHERE state="approved"
-    GROUP BY month,country
-),cb_by_month AS(
-    SELECT DATE_FORMAT(c.trans_date,"%Y-%m") AS month,
-    t.country,
-    count(t.country) AS chargeback_count,
-    sum(t.amount) AS chargeback_amount
-    FROM Chargebacks c
-    JOIN Transactions t
+    WHERE state='approved'
+    GROUP BY 1,2
+), cb_summary AS(
+    SELECT
+        DATE_FORMAT(c.trans_date,'%Y-%m') AS month,
+        t.country,
+        count(c.trans_id) AS chargeback_count,
+        sum(t.amount) AS chargeback_amount
+    FROM Transactions t
+    JOIN Chargebacks c
     ON t.id=c.trans_id
-    GROUP BY month,t.country
+    GROUP BY 1,2
 )
 
-SELECT t.month, t.country,
+SELECT
+    t.month,
+    t.country,
     t.approved_count,
     t.approved_amount,
-    COALESCE(c.chargeback_count,0) AS chargeback_count,
-    COALESCE(c.chargeback_amount,0) AS chargeback_amount
-FROM trans_by_month t
-LEFT JOIN cb_by_month c
-ON t.month=c.month AND t.country=c.country
-
+    IFNULL(c.chargeback_count,0) AS chargeback_count,
+    IFNULL(c.chargeback_amount,0) AS chargeback_amount
+FROM trans_summary t
+LEFT JOIN cb_summary c
+USING(month,country)
 UNION
-
-SELECT c.month, c.country,
-    COALESCE(t.approved_count,0),
-    COALESCE(t.approved_amount,0),
+SELECT
+    c.month,
+    c.country,
+    IFNULL(t.approved_count,0) AS approved_count,
+    IFNULL(t.approved_amount,0) AS approved_amount,
     c.chargeback_count,
     c.chargeback_amount
-FROM trans_by_month t
-RIGHT JOIN cb_by_month c
-ON t.month=c.month AND t.country=c.country
+FROM cb_summary c
+LEFT JOIN trans_summary t
+USING(month,country)
 
 /* Solution 2
 by ZacksAmber
@@ -50,28 +54,34 @@ by ZacksAmber
 2. use if function create new columns
 */
 
-WITH CTE AS
-	(
-	SELECT
-		id, country, state, amount,
-		DATE_FORMAT(trans_date, '%Y-%m') AS month
-	FROM Transactions
-	WHERE state = 'approved'
-	UNION
-	SELECT
-		c.trans_id, t.country, 'chargeback' AS state, t.amount,
-		DATE_FORMAT(c.trans_date, '%Y-%m') AS month
-	FROM Chargebacks c
-	LEFT JOIN Transactions t ON c.trans_id = t.id
-	)
-# SELECT * FROM CTE;
+WITH summary AS (
+    SELECT
+        id,
+        country,
+        state,
+        amount,
+        trans_date
+    FROM Transactions
+    WHERE state='approved'
+    UNION
+    SELECT
+        c.trans_id AS id,
+        t.country,
+        'chargeback' AS state,
+        t.amount,
+        c.trans_date
+    FROM Chargebacks c
+    LEFT JOIN Transactions t
+    ON t.id=c.trans_id
+)
+
 SELECT
-	month,
+    DATE_FORMAT(trans_date,'%Y-%m') AS month,
     country,
-    SUM(IF(state='approved', 1, 0)) AS approved_count,
-    SUM(IF(state='approved', amount, 0)) AS approved_amount,
-    SUM(IF(state='chargeback', 1, 0)) AS chargeback_count,
-    SUM(IF(state='chargeback', amount, 0)) AS chargeback_amount
-FROM CTE
-GROUP BY month, country
-ORDER BY month, country;
+    sum(IF(state='approved',1,0)) AS approved_count,
+    sum(IF(state='approved',amount,0)) AS approved_amount,
+    sum(IF(state='chargeback',1,0)) AS chargeback_count,
+    sum(IF(state='chargeback',amount,0)) AS chargeback_amount
+
+FROM summary
+GROUP BY 1,2;
