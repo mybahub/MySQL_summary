6eea8f2b70a5ef55460c77649ec5a55e5c78f25f
mybahub <enochyn@outlook.com>
2021-10-16 20:34:51 -0700

    updat 1212

--- 1001-1300/1212.Team_scores_in_football_tournament.sql
+++ 1001-1300/1212.Team_scores_in_football_tournament.sql
@@ -31,3 +31,21 @@ LEFT JOIN summary s
 USING (team_id)
 GROUP BY t.team_id
 ORDER BY num_points DESC,t.team_id;
+
+
+/* Solution 2*/
+SELECT
+    t.team_id,
+    t.team_name,
+    IFNULL(sum(
+        CASE
+        WHEN t.team_id=m.host_team AND host_goals>guest_goals THEN 3
+        WHEN t.team_id=m.guest_team AND host_goals<guest_goals THEN 3
+        WHEN host_goals=guest_goals THEN 1
+        ELSE 0 END
+    ) ,0) AS num_points
+FROM Teams t
+LEFT JOIN Matches m
+ON t.team_id=m.host_team OR t.team_id=m.guest_team
+GROUP BY t.team_id
+ORDER BY num_points DESC,t.team_id;
