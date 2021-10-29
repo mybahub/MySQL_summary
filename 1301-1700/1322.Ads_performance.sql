c90196ff76a7769a1ad2b3cc2ac2b85c443eefc6
mybahub <enochyn@outlook.com>
2021-10-28 21:44:04 -0700

    update 1322

--- 1301-1700/1322.Ads_performance.sql
+++ 1301-1700/1322.Ads_performance.sql
@@ -1,6 +1,20 @@
-/* easy */
+/* easy
+https://leetcode.com/problems/ads-performance/
+*/
 
-/* Solution 1
+
+
+/* Solution 1*/
+
+SELECT
+    ad_id,
+    ifnull(round(sum(if(action="Clicked",1,0))/ sum(if(action="Clicked" or action="Viewed",1,0))*100,2),0) AS ctr
+FROM Ads
+GROUP BY ad_id
+ORDER BY ctr DESC, ad_id
+
+
+/* Solution 2
 if function break one column to many
 */
 WITH action_dum AS (
